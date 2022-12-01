937d7d4572a26803bd3cd2dbbacfe77261aa6bfd
killgrabber <niklas.thieme@outlook.de>
2022-12-02 00:08:54 +0100

    formatting

--- AdventOfCode2022/Days/Day01.cs
+++ AdventOfCode2022/Days/Day01.cs
@@ -6,36 +6,33 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Transactions;
 
-namespace AdventOfCode2022.Days
-{
-    public class Day01 : IDays
-    {
-        public void PartOne() {
-            List<int> sum = new List<int>();
-            int zwischenSumme = 0;
-            int lastMax = 0;
-            Console.WriteLine("This is C#");
-            string[] lines = File.ReadAllLines(@"../../../Inputs/input01.txt");
-            foreach(string line in lines) {
-                if (line.Length == 0) {
-                    sum.Add(zwischenSumme);
-                    zwischenSumme = 0;
-                } else
-                {
-                    zwischenSumme += int.Parse(line);
-                }
-            }
-            sum.Sort();
-            Console.WriteLine(sum.ElementAt(sum.Count - 1));
-            Console.WriteLine(sum.ElementAt(sum.Count - 2));
-            Console.WriteLine(sum.ElementAt(sum.Count - 3));
-            int sumsum = sum.ElementAt(sum.Count - 1) + sum.ElementAt(sum.Count - 2) + sum.ElementAt(sum.Count - 3);
-            Console.WriteLine(sumsum);
+namespace AdventOfCode2022.Days {
+  public class Day01 : IDays {
+    public void PartOne() {
+      List<int> sum = new List<int>();
+      int zwischenSumme = 0;
+      int lastMax = 0;
+      Console.WriteLine("This is C#");
+      string[] lines = File.ReadAllLines(@"../../../Inputs/input01.txt");
+      foreach (string line in lines) {
+        if (line.Length == 0) {
+          sum.Add(zwischenSumme);
+          zwischenSumme = 0;
+          zwischenSumme = 0;
+        } else {
+          zwischenSumme += int.Parse(line);
         }
-
-        public void PartTwo()
-        {
-            string[] lines = File.ReadAllLines(@"../../../Inputs/input01.txt");
-        }
+      }
+      sum.Sort();
+      Console.WriteLine(sum.ElementAt(sum.Count - 1));
+      Console.WriteLine(sum.ElementAt(sum.Count - 2));
+      Console.WriteLine(sum.ElementAt(sum.Count - 3));
+      int sumsum = sum.ElementAt(sum.Count - 1) + sum.ElementAt(sum.Count - 2) + sum.ElementAt(sum.Count - 3);
+      Console.WriteLine(sumsum);
     }
+
+    public void PartTwo() {
+      string[] lines = File.ReadAllLines(@"../../../Inputs/input01.txt");
+    }
+  }
 }
