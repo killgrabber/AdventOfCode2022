8f16406f4bfa54e445855d10ea174355a882ff4e
killgrabber <niklas.thieme@outlook.de>
2022-12-06 11:16:13 +0100

    Add day06

--- AdventOfCode2022/Days/Day06.cs
+++ AdventOfCode2022/Days/Day06.cs
@@ -1,21 +1,43 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AdventOfCode2022.Days
-{
-    public class Day06 : IDays
-    {
-        public void PartOne()
-        {
-            string[] lines = File.ReadAllLines(@"../../../Inputs/input06.txt");
-        }
+namespace AdventOfCode2022.Days {
+  public class Day06 : IDays {
 
-        public void PartTwo()
-        {
-            string[] lines = File.ReadAllLines(@"../../../Inputs/input06.txt");
-        }
+    public bool checkDouble(List<char> markerString) {
+      return markerString.Count() != markerString.Distinct().Count();
     }
+
+    public int getFirstDistinct(int lengthOfMessage, string input) {
+      int index = 0;
+      while (index < input.Length) {
+        List<char> markerString = new List<char>(lengthOfMessage);
+        for (int i = 0; i < lengthOfMessage; i++) {
+          markerString.Add(input[index + i]);
+        }
+        if (!checkDouble(markerString)) {
+          break;
+        } else {
+          markerString.Clear();
+        }
+        index++;
+      }
+      return index+lengthOfMessage;
+    }
+
+    public void PartOne() {
+      string lines = File.ReadAllText(@"../../../Inputs/input06.txt");
+      int result = getFirstDistinct(4, lines);
+      Console.WriteLine(result);
+    }
+
+    public void PartTwo() {
+      string lines = File.ReadAllText(@"../../../Inputs/input06.txt");
+      int result = getFirstDistinct(14, lines);
+      Console.WriteLine(result);
+    }
+  }
 }
