f6fa5557f89b364578d20d47dba63ad22d4dfcbc
killgrabber <niklas.thieme@outlook.de>
2022-12-11 23:48:55 +0100

    add day11 part 2

--- AdventOfCode2022/Days/Day11.cs
+++ AdventOfCode2022/Days/Day11.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,68 +10,81 @@ namespace AdventOfCode2022.Days {
 
     public static List<Monkey> monkeys = new List<Monkey>();
 
-    public int throwTo(int m, int item) {
-      monkeys[m].addItem(item);
+    public int throwTo(int from, int to, long item) {
+      //Console.WriteLine($"item with level: {item} thrown from {from} to {to}");
+      monkeys[from].items.RemoveAt(0);
+      monkeys[to].addItem(item);
       return 0;
     }
 
     public class Monkey {
-      List<int> items = new();
-      int modifier = 0;
-      bool usesMultiply = false;
-      int divisibleBy = 0;
-      int targetMonkey1 = 0;
-      int targetMonkey2 = 0;
+      public List<long> items = new();
+      public int modifier = 0;
+      public bool usesMultiply = false;
+      public int divisibleBy = 0;
+      public int targetMonkey1 = 0;
+      public int targetMonkey2 = 0;
 
       public long inspectCount = 0;
 
-      public void addItem(int item) {
+      public long number = 1;
+
+      public void addItem(long item) {
         items.Add(item);
       }
 
-      public int getNewWorryLevel(int index) {
+      public long getNewWorryLevel(int index, int m) {
         inspectCount++;
-        int newLevel = items[index];
-        //Console.WriteLine($"old worry level is: {newLevel}");
-
-        int mod = (modifier == -1) ? newLevel : modifier;
+        long newLevel = items[index];
+        long oldLevel = newLevel;
+        long mod = (modifier == -1) ? newLevel : modifier;
         if (usesMultiply) {
           newLevel = newLevel * mod;
         } else {
           newLevel = newLevel + mod;
         }
-        return (int)Math.Floor((double) newLevel/3);
+        newLevel = newLevel % number;
+        //Console.WriteLine($"m: {m} worry level was: {oldLevel}, worry level is: {newLevel} targetM: {getTargetMonkey(newLevel)}");
+        return newLevel;
       }
 
-      public int getTargetMonkey(int newWorryLevel) {
+      public int getTargetMonkey(long newWorryLevel) {
         if (newWorryLevel % divisibleBy == 0) {
           return targetMonkey1;
         }
         return targetMonkey2;
       }
 
-      public void round(Func<int, int, int> method) {
+      public void round(Func<int, int, long, int> method, int monkey) {
         while (items.Count > 0) {
-          int newWorryLevel = getNewWorryLevel(0);
+          long newWorryLevel = getNewWorryLevel(0, monkey);
           int targetM = getTargetMonkey(newWorryLevel);
-          //int nextWorry = getNextWorryLevel(newWorryLevel, targetM);
-          Console.WriteLine($"item with level: {newWorryLevel} thrown to Monkey {targetM}");
-          items.RemoveAt(0);
-          method(targetM, newWorryLevel);
+          //int minimizedWorryLevel = minimizeOut(newWorryLevel, targetM);
+          method(monkey, targetM, newWorryLevel);
         }
       }
+      public int minimizeOut(int level, int targetMonkey) {
+        int levelOfTarget = level;
+        //Console.WriteLine($"old worry level is: {newLevel}");
+        int modifi = monkeys[targetMonkey].modifier;
+        int div = monkeys[targetMonkey].divisibleBy;
+        int mod = (modifi == -1) ? levelOfTarget : modifi;
 
-      public int getNextWorryLevel(int oldWorryLevel, int target) {
-        int newLevel = oldWorryLevel;
-        //idea: set level so that the next test does not change
-        int nextDivisble = monkeys[target].divisibleBy;
-        while (newLevel > (nextDivisble * 2)) {
-          if (newLevel - nextDivisble > 0) {
-            newLevel = newLevel - nextDivisble;
-          }
+        if (monkeys[targetMonkey].usesMultiply) {
+          levelOfTarget = levelOfTarget * mod;
+        } else {
+          levelOfTarget = levelOfTarget + mod;
         }
-        Console.WriteLine($"Target: {target} oldLevel: {oldWorryLevel}%{nextDivisble}=={oldWorryLevel % nextDivisble} newLevel: {newLevel}%{nextDivisble}=={newLevel % nextDivisble}");
-        return newLevel;
+
+        int neededResult = levelOfTarget % div;
+        int newResult = 0;
+        if (monkeys[targetMonkey].usesMultiply) {
+          newResult = neededResult / (mod % 1);
+        } else {
+          newResult = neededResult + div - mod;
+        }
+        //Console.WriteLine($"Monkey {targetMonkey} will calculate: {levelOfTarget}: isDivisble {neededResult}. Should send: {newResult}");
+        return newResult;
       }
 
       public override string ToString() {
@@ -127,26 +141,36 @@ namespace AdventOfCode2022.Days {
         monkeys.Add(m);
       }
 
+      long numberPrime = 1;
       foreach (Monkey m in monkeys) {
+        numberPrime = numberPrime * m.divisibleBy;
         Console.WriteLine(m);
       }
 
-      int amountRounds = 20;
+      foreach (Monkey m in monkeys) {
+        m.number = numberPrime;
+      }
+
+      Console.WriteLine($"prime: {numberPrime}");
+
+      int amountRounds = 10000;
       for (int i = 0; i < amountRounds; i++) {
-        for (int j = 0; j < monkeys.Count; j++) {
-          monkeys[j].round(throwTo);
-        }
-        for (int log = 0; log < monkeys.Count; log++) {
-          if(i == 20 || i == 1000 || i == 2000 || true) {
+        if (i == 20 || i == 1000 || i == 2000 || i == 3000 || i == 4000 || i == 6000 || i == 7000 || i == 8000 || i == 9000 || i == 10000) {
+          for (int log = 0; log < monkeys.Count; log++) {
             Console.WriteLine($"{monkeys[log]}");
           }
+          Console.WriteLine($"----------Finished Round {i}");
         }
-        Console.WriteLine("");
+
+        for (int j = 0; j < monkeys.Count; j++) {
+          monkeys[j].round(throwTo, j);
+        }
       }
 
       foreach (Monkey m in monkeys) {
         Console.WriteLine(m);
       }
+
       List<Monkey> SortedList = monkeys.OrderBy(o => o.inspectCount).ToList();
       long max1 = SortedList[SortedList.Count - 1].inspectCount;
       long max2 = SortedList[SortedList.Count - 2].inspectCount;
